ed6cbd5cb530494cd44cd75ac08690f872003941
paulinaMorenoA <105310671+paulinaMorenoA@users.noreply.github.com>
2022-05-13 20:19:47 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.85"
+      version = "~> 4.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.85"
+      version = "~> 4.1"
     }
   }
 }
